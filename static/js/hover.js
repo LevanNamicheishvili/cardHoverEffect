f69457a74644eb8a4024280f853af965122b95fd
LevanNamicheishvili <117360031+LevanNamicheishvili@users.noreply.github.com>
2023-02-08 15:22:20 +0400

    Add files via upload

---
+++ static/js/hover.js
@@ -0,0 +1,7 @@
+let myAnimation = new hoverEffect({
+    parent: document.querySelector('.wrapper'),
+    intensity: 0.5,
+    image1: 'static/img/img_one.jpg',
+    image2: 'static/img/img_two.jpg',
+    displacementImage: 'static/img/4.png'
+});
